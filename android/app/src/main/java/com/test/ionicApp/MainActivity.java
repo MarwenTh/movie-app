f7d1ce6ef74a69da19013603a665d4108005256d
MarwenTh <marwentahouri2@gmail.com>
2023-10-30 13:10:07 +0100

    First Commit

---
+++ android/app/src/main/java/com/test/ionicApp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.test.ionicApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app',
-  webDir: 'dist',
+  appId: "com.test.ionicApp",
+  appName: "ionic-app",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
